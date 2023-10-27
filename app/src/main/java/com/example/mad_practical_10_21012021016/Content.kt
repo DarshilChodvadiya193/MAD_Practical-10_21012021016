102aa61841cc0775b0661539cf82f0cfca1ea66b
student <student@gnu.ac.in>
2023-10-27 07:58:17 +0530

    Practical-10

--- app/src/main/java/com/example/mad_practical_10_21012021016/Content.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mad_practical_10_21012021016
-
-import java.io.Serializable
-
-class Contact(val id:String , var name:String, var emailid:String, var PhoneNo:String,var address:String,var latitude:Double,var longitude:Double):Serializable {
-}
